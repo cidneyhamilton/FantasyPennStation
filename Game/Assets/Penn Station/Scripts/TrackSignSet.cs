e4d913e4d1aa78e67625e93c4b87f1ae6dd5223d
Cidney Hamilton <cidney@cidneyhamilton.com>
2023-02-03 17:32:01 -0500

    Clean up spacing between tracks.

--- Game/Assets/Penn Station/Scripts/TrackSignSet.cs
+++ Game/Assets/Penn Station/Scripts/TrackSignSet.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class TrackSignSet : MonoBehaviour
 {
     // Defines the space between each sign
-    public float SpaceBetween = 20;
+    public float SpaceBetweenSigns = 20;
     
     // The  number of signs to spawn for each set of signs
-    public float NumberOfSigns = 3;
+    public int NumberOfSignsPerInterval = 3;
 
+    // The number of sets of signs
+    public int NumberOfIntervals = 3;
+    
+    public float SpaceBetweenIntervals = 160;
+    
     public Signpost SignPrefab;
 
     // The material to apply to the sign
@@ -23,19 +28,29 @@ public class TrackSignSet : MonoBehaviour
     // TODO: Invoke this from an editor script to show in scene view
     protected void SpawnSigns()
     {
-        for (int i = 0; i < NumberOfSigns; i++)
-        {
-            // Spawn a new prefab with defined spacing
-            Vector3 position = Vector3.zero;
-            position.x += i * SpaceBetween;
+        Vector3 position = Vector3.zero;
+
+        for (int j = 0; j < NumberOfIntervals; j++)
+        {            
+            
+            for (int i = 0; i < NumberOfSignsPerInterval; i++)
+            {
+
+                // Spawn a new prefab with defined spacing
+                Signpost sign = Instantiate(SignPrefab, position, Quaternion.identity) as Signpost;            
+
+                sign.transform.SetParent(transform, false);
+
+                sign.ApplyMaterial(signMaterial);
+
+                
+
+                position.x += SpaceBetweenSigns;
+                
                         
-            Signpost sign = Instantiate(SignPrefab, position, Quaternion.identity) as Signpost;            
+            }
 
-            sign.transform.SetParent(transform, false);
-
-            sign.ApplyMaterial(signMaterial);
-            
-            
+            position.x += SpaceBetweenIntervals;            
         }
         
     }
