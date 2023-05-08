4fb591ae76c869a69600c2e603fff6a7237b66e3
Cidney Hamilton <cidney@cidneyhamilton.com>
2023-05-08 17:04:02 +0200

    Add script to test on different target frame rates.

---
+++ Game/Assets/Penn Station/Scripts/TargetFrameRate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+
+		public int target = 60;
+		float currentFrameTime;
+		
+    // Start is called before the first frame update
+    void Start()
+    {
+				QualitySettings.vSyncCount = 0;
+				Application.targetFrameRate = 9999;
+        currentFrameTime = Time.realtimeSinceStartup;
+        StartCoroutine("WaitForNextFrame");
+    }
+
+    IEnumerator WaitForNextFrame()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            currentFrameTime += 1.0f / target;
+            var t = Time.realtimeSinceStartup;
+            var sleepTime = currentFrameTime - t - 0.01f;
+            if (sleepTime > 0)
+                Thread.Sleep((int)(sleepTime * 1000));
+            while (t < currentFrameTime)
+                t = Time.realtimeSinceStartup;
+        }
+    }
+  
+}
