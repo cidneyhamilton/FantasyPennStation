9265892a04aa8ada3c7f94dea479993be6ef69c0
Cidney Hamilton <cidney@cidneyhamilton.com>
2023-05-26 20:10:16 +0200

    Move the player with the escalator when they're on it.

---
+++ Game/Assets/Penn Station/Scripts/Escalator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escalator : MonoBehaviour
+{
+		public Vector3 MovementDirection;
+		public GameObject Staircase;
+		
+		private GameObject _player;
+		private bool _onEscalator;
+
+		void Start()
+		{
+				Debug.Log($"Staircase rotation: {Staircase.transform.rotation.eulerAngles.y}");
+				if (Staircase.transform.rotation.eulerAngles.y != 180)
+				{
+						Debug.Log("Flipping staircase.");
+						// Flip movement direction						
+						MovementDirection = new Vector3(MovementDirection.x * -1, MovementDirection.y, MovementDirection.z);
+				}
+		}
+		void OnTriggerEnter(Collider other)
+		{
+				if (other.tag == "Player")
+				{
+						Debug.Log("Player entering escalator.");
+						_player = other.gameObject;
+						_onEscalator = true;
+				}
+		}
+
+		void OnTriggerExit(Collider other)
+		{
+				if (other.tag == "Player")
+				{
+						// Debug.Log("Player exiting escalator.");
+						_player = null;
+						_onEscalator = false;
+				}
+		}
+
+		void FixedUpdate()
+		{
+				if (_onEscalator && _player)
+				{
+						_player.transform.Translate(MovementDirection * Time.deltaTime, Space.World);
+				}
+		}
+}
